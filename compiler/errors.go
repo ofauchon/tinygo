80fa867afae774165e0a1dc186592ef033ecb885
Ayke van Laethem <aykevanlaethem@gmail.com>
2019-11-21 14:20:12 +0100

    compiler: refactor IR generation
    This is the first commit in a series to refactor the compiler. The
    intention is to make sure every function to be compiled eventually has
    its own IR builder. This will make it much easier to do other
    refactorings in the future:
    
      * Most code won't depend (directly) on the central Compiler object,
        perhaps making it possible to eliminate it in the future. Right now
        it's embedded in the `builder` struct but individual fields from the
        `Compiler` can easily be moved into the `builder` object.
      * Some functions are not directly exposed in Go SSA, they are wrapper
        functions for something. At the moment they are included in the list
        of functions to be compiled with the reachability analysis
        (SimpleDCE) in the ir package, but eventually this reachability
        analys will be removed. At that point, it would be very convenient
        to be able to simply build a function with a new IR builder.
    
    The `compilerContext` struct makes sure that it is not possible for
    `builder` methods to accidentally use global state such as the global IR
    builder. It is a transitional mechanism and may be removed when
    finished.

--- compiler/errors.go
+++ compiler/errors.go
@@ -1,11 +1,14 @@
 package compiler
 
+// This file contains some utility functions related to error handling.
+
 import (
 	"go/token"
 	"go/types"
 )
 
-func (c *Compiler) makeError(pos token.Pos, msg string) types.Error {
+// makeError makes it easy to create an error from a token.Pos with a message.
+func (c *compilerContext) makeError(pos token.Pos, msg string) types.Error {
 	return types.Error{
 		Fset: c.ir.Program.Fset,
 		Pos:  pos,
